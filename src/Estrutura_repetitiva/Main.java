0458d0e022e4818c2c77bf43f155fc29ff85820f
Erick-Varjao <erickvarjao24@gmail.com>
2025-03-06 23:54:31 -0300

    Aprendendo Estrutura repetitiva while

---
+++ src/Estrutura_repetitiva/Main.java
@@ -0,0 +1,28 @@
+package Estrutura_repetitiva;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		
+		int x;
+		System.out.println("Digite um número: ");
+		x = sc.nextInt();
+		
+		int soma = 0;
+		
+		while (x != 0) {
+			soma = soma + x;
+			System.out.println("Digite novamente");
+			x = sc.nextInt();
+		}
+		System.out.println("Você conseguiu!!!");
+		System.out.println("Resultado da soma dos números digitados: " + soma);
+		
+		sc.close();}
+
+}
